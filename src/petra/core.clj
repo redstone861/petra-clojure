32a7698f7a84cc216441e716e8a1433b69191a51
Sam Cousins <100026793@mvla.net>
2025-11-27 17:26:56 -0800

    add selection

--- src/petra/core.clj
+++ src/petra/core.clj
@@ -11,27 +11,36 @@
 (def D :D)
 (def P :P)
 
-(defn psel
-  "parse selectional frame"
-  [sel cg]
-  (map 
-   #(identity {:cat (:cat %) :head (get % :head false) :role (get % :role nil)}) 
-   (map 
-    #(let [head? (= % :_)] 
-       (if (seqable? %) % {:cat (if head? cg %) :head head?})) 
-    (map 
-     #(if (sequential? %) {:cat (first %) :role (second %)} %) 
-     sel)))
+(defn psel-single [x]
+  (let [bmap 
+        (if (map? x) 
+          x
+          (if (vector? x)
+            {:cat (first x) :role (second x)}
+            {:cat x}))]
+    (merge {:cat nil :role nil} bmap))
+)
+
+(defn tag-opt [sels]
+  (let [make-map (fn [x]
+                   (let [sing (psel-single (if (seq? x) (first x) x))] 
+                     (assoc sing :opt (or (seq? x) false))))]
+    (apply vector (map make-map sels)))
+)
+
+(defn psel [sels cg]
+  (let [parsed (tag-opt sels)]
+    (mapv #(let [head? (= (:cat %) :_)] (merge % (if head? {:cat cg} {}) {:head head?}))
+          parsed))
+)
+
+(defn get-head [so]
+  (first (filter :head so))
 )
 
 (defn label
   [so]
-  (first (filter :head so))
-)
-
-(defn entry
-  [lex cg sel]
-  {:lex lex :cat cg :sel (psel sel cg)}
+  (:cat (get-head so))
 )
 
 (defn mrg
@@ -39,15 +48,35 @@
   (seq sos)
 )
 
-(defn lex
-  [& entries]
-  (map #(apply entry %) entries)
+(defn sel-sat?
+  [so]
+  (let [sel (:sel (get-head so))]
+    (mapv #(= (:cat %1) (:cat %2)) sel so))
+)
+
+(defn entry
+  [lx cg sel]
+  {:lex lx :cat cg :sel (psel sel cg)}
+)
+
+(defn lex 
+  [& entries] 
+  (map #(apply entry %) entries) 
+)
+
+(defn lexer
+  [strin lx]
+  (map 
+   #(first (filter (fn [ent] (= % (:lex ent))) lx))
+   strin
+   )
 )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
+  (println (psel [:_ :A '([:B :DO])] :L))
   (entry "eat" V [:_ N])
   (mrg 1 2)
 )
